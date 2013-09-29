07a92d225a3f591414458dca8a6c46f40df1fd6a
Freddie Fujiwara <github@ze.gs>
2013-09-29 19:30:43 +0900

    add urls and validations

--- lib/fluent/plugin/in_json_api.rb
+++ lib/fluent/plugin/in_json_api.rb
@@ -2,14 +2,24 @@ module Fluent
     class JsonApiInput < Input
         Plugin.register_input('json_api', self)
 
-        config_param :url,      :string,  :default => 'http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=#page#'
+        config_param :url,      :string,  :default => 'http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=__PAGE__'
         config_param :max_page, :integer, :default =>  1000
         config_param :tag,      :string
 
+        attr_reader :urls
         def initialize
             super
             require 'json'
             require 'net/http'
+            @urls = []
+        end
+
+        def configure(config)
+            super
+            raise Fluent::ConfigError unless ['http','https'].include? URI.parse(config['url']).scheme
+            for page in 1 .. config['max_page'].to_i
+                @urls.push config['url'].gsub('__PAGE__',page.to_s)
+            end
         end
 
         def start
--- test/plugin/in_json_api.rb
+++ test/plugin/in_json_api.rb
@@ -7,8 +7,8 @@ class FileInputTest < Test::Unit::TestCase
         Fluent::Test.setup
 
         @d = create_driver %[
-            url    localurl
-            max_page    6379
+            url   http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=__PAGE__
+            max_page    1000
             tag     input.json
         ]
         @time = Time.now.to_i
@@ -19,8 +19,38 @@ class FileInputTest < Test::Unit::TestCase
     end
 
     def test_configure
-        assert_equal 'localurl'   , @d.instance.url
-        assert_equal 6379          , @d.instance.max_page
+        assert_equal 'http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=__PAGE__'   , @d.instance.url
+        assert_equal 1000          , @d.instance.max_page
         assert_equal 'input.json', @d.instance.tag
+        [ %[
+                url   hoge
+                max_page    1000
+                tag     input.json
+            ],
+          %[
+                url   ftp://hoge.com
+                max_page    1000
+                tag     input.json
+            ],
+          %[
+                url   http://hoge.com
+                max_page    1000
+            ],
+          %[
+                url   http://hoge.com
+                max_page    hoge
+            ]
+        ].each do |config|
+            assert_raise Fluent::ConfigError do
+                create_driver config
+            end
+        end
+    end
+    def test_urls
+        page = 1
+        @d.instance.urls.each do |url|
+            assert_equal "http://pipes.yahoo.com/pipes/pipe.run?_id=c9b9df32b4c3e0ccbe4547ae7e00ed2f&_render=json&condition=d7D&genre=100533&page=#{page}", url
+            page += 1
+        end
     end
 end
