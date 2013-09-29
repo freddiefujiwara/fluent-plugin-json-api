b5ff95f00ee07ff8267d703f7768d47a153e2dd1
Freddie Fujiwara <github@ze.gs>
2013-09-29 17:19:26 +0900

    first commit

---
+++ lib/fluent/plugin/json/api.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/json/api/version"
+
+module Fluent
+  module Plugin
+    module Json
+      module Api
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/json/api/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Plugin
+    module Json
+      module Api
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
